3851ab54152152b26ce70ace3ffd1d6d420d7911
Viktor <viktor@farcic.com>
2017-07-31 01:14:57 +0200

    Instrumentation

---
+++ scripts/dm-swarm-11.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+./scripts/dm-swarm.sh
+
+eval $(docker-machine env swarm-1)
+
+docker network create -d overlay proxy
+
+docker stack deploy \
+    -c stacks/docker-flow-proxy-mem.yml \
+    proxy

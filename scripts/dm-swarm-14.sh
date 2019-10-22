593f704323fba774f9a2ed6adc3f5771d8a663ea
Alessandro Affinito <alessandro.affinito@oracle.com>
2019-10-22 18:26:04 +0200

    Issue #71

---
+++ scripts/dm-swarm-14.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+./scripts/dm-swarm.sh
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+eval $(docker-machine env swarm-1)
+
+docker network create -d overlay --attachable proxy
+docker stack deploy -c stacks/docker-flow-proxy.yml proxy
+
+docker network create -d overlay --attachable monitor
+
+
+###########################################################################
+#   PROMETHEUS MONITORING
+#
+#  Install "Incoming WebHooks" Slack App first, then save your web hook
+#   This API will be used as a default receiver by the alert_manager
+SERVICE_NAME='go-demo_main'
+SLACK_API_URL='https://hooks.slack.com/services/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'
+SLACK_TOKEN='DevOps22'
+
+echo "route:
+  group_by: [service,scale]
+  repeat_interval: 5m
+  group_interval: 5m
+  receiver: 'slack'
+  routes:
+  - match:
+      service: '${SERVICE_NAME}'
+      scale: 'up'
+    receiver: 'jenkins-${SERVICE_NAME}-up'
+  - match:
+      service: '${SERVICE_NAME}'
+      scale: 'down'
+    receiver: 'jenkins-${SERVICE_NAME}-down'
+
+receivers:
+  - name: 'slack'
+    slack_configs:
+      - send_resolved: true
+        title: '[{{ .Status | toUpper }}] {{ .GroupLabels.service }} service is in danger!'
+        title_link: 'http://$(docker-machine ip swarm-1)/monitor/alerts'
+        text: '{{ .CommonAnnotations.summary}}'
+        api_url: ${SLACK_API_URL}
+  - name: 'jenkins-${SERVICE_NAME}-up'
+    webhook_configs:
+      - send_resolved: false
+        url: 'http://$(docker-machine ip swarm-1)/jenkins/job/service-scale/buildWithParameters?token=${SLACK_TOKEN}&service=${SERVICE_NAME}&scale=1'
+  - name: 'jenkins-${SERVICE_NAME}-down'
+    webhook_configs:
+      - send_resolved: false
+        url: 'http://$(docker-machine ip swarm-1)/jenkins/job/service-scale/buildWithParameters?token=${SLACK_TOKEN}&service=${SERVICE_NAME}&scale=-1'
+" | docker secret create alert_manager_config -
+
+DOMAIN=$(docker-machine ip swarm-1)
+
+docker stack deploy \
+    -c stacks/docker-flow-monitor-slack.yml \
+    monitor
+
+echo "admin" | docker secret create jenkins-user -
+echo "admin" | docker secret create jenkins-pass -
+
+export SLACK_IP=`ping -c 1 devops20.slack.com | awk -F '[()]' '/PING/{print $2}'`
+
+docker stack deploy -c stacks/jenkins-scale.yml jenkins
+sleep 2
+docker stack ps jenkins
+echo
+echo "Configure Jenkins jobs at: "
+echo "http://$(docker-machine ip swarm-1)/jenkins/job/service-scale/configure"
+echo "Prometheus alert screen at: http://$(docker-machine ip swarm-1)/monitor/alerts"
+echo
+
+docker stack deploy -c stacks/go-demo-instrument-alert-error.yml go-demo
+
+echo "Please wait.. "
+sleep 5
+docker service ls
+
+echo
+echo " Web service exposed at http://${DOMAIN}/demo/hello"
+echo " Please now copy conf/jenkins/service-scale.groovy in http://${DOMAIN}/jenkins/job/service-scale/ \
+        configuration and check if parameters (service, scale) are accepted"
--- scripts/dm-swarm.sh
+++ scripts/dm-swarm.sh
@@ -1,23 +1,75 @@
 #!/usr/bin/env bash
+# Exit When Any Command Fails
+set -e -u
+# set debugging
+trap 'echo -e "\v>> ERROR: Be sure that none of the steps fail while starting the cluster.. "' EXIT
+set -x
+
+docker -v
+docker-machine -v
+vboxmanage --version
+echo
+
+####------ USER
+# sudo usermod -aG docker $USER
 
 if [[ "$(uname -s )" == "Linux" ]]; then
   export VIRTUALBOX_SHARE_FOLDER="$PWD:$PWD"
 fi
 
-for i in 1 2 3; do
-    docker-machine create \
-        -d virtualbox \
-        swarm-$i
-done
+if [ $(systemctl is-active virtualbox) != 'active' ]; then 
+  service virtualbox start
+fi
+###########################################################################
+###         MACHINES              #########################################
+# creates machines with virtualbox driver
+MACHINES_CREATED_NUM=`docker-machine ls --quiet --filter driver=virtualbox | wc -l`
+MACHINES_READY_NUM=`docker-machine ls --quiet --filter driver=virtualbox --filter state=Running | wc -l`
+MACHINES_STOPPED=`docker-machine ls --quiet --filter driver=virtualbox --filter state=Stopped --filter state=Saved`
 
+echo
+echo "Checking for Docker virtualBox machines.. "
+
+if [[ $MACHINES_CREATED_NUM == 0 ]]; then
+  for i in 1 2; do
+    docker-machine create -d virtualbox --virtualbox-cpu-count 2 swarm-$i
+  done
+
+# if they have been already created
+elif [[ $MACHINES_READY_NUM == 0 ]]; then
+  # machines were correctly stopped
+  for machine in $(seq 1 $MACHINES_STOPPED); do
+    docker-machine start $machine
+  done
+
+  export MANAGER_IP=$(docker-machine ip swarm-1):2377
+  docker-machine env swarm-1
+  eval $(docker-machine env swarm-1)
+  echo "The machines are already Running.."
+
+else
+  # if machines are in timeout
+  for i in $(seq 1 $MACHINES_CREATED_NUM); do
+    docker-machine stop  swarm-$i
+    docker-machine start swarm-$i
+    docker-machine regenerate-certs swarm-$i
+  done
+  echo "Wait for machines to start please.."
+
+fi
+
+sleep 5
+docker-machine ls
+
+# Connect shell to the manager machine
 eval $(docker-machine env swarm-1)
 
-docker swarm init \
-  --advertise-addr $(docker-machine ip swarm-1)
+# Promote first machine as manager
+docker swarm init --advertise-addr $(docker-machine ip swarm-1)
 
+# Add workers to the swarm 
 TOKEN=$(docker swarm join-token -q manager)
-
-for i in 2 3; do
+for i in 2; do
     eval $(docker-machine env swarm-$i)
 
     docker swarm join \
@@ -25,5 +77,29 @@ for i in 2 3; do
         --advertise-addr $(docker-machine ip swarm-$i) \
         $(docker-machine ip swarm-1):2377
 done
+
+export MANAGER_IP=$(docker-machine ip swarm-1):2377
+docker-machine env swarm-1
+
+
+## ----- Exchange SSH keys --------
+# user: docker
+# psw:  tcuser
+echo " Exchanging certificate keys with docker machines.."
+echo
+ssh-keygen -t rsa -b 2048
+#   put the certificate on the virtual machine
+for i in 1 2; do
+  ssh-copy-id docker@${docker machine ip swarm-$i} 
+done
+## --------------------------------
+
+## -----Env setup------------------
+for i in 2; do
+  eval $(docker-machine env swarm-$i)
+  export CURRENT_TIME=$(date +%H:%M:%S)
+  cat setup/setup-docker-machine.sh | ssh docker@$(docker-machine ip swarm-$i)
+done
+## ----------------------------------
 
 echo ">> The swarm cluster is up and running"
